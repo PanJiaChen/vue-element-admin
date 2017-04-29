3063d9c86511a5b91ad0d27e40c41d06f88939b4
Pan <panfree23@gmail.com>
2017-04-29 16:36:14 +0800

    replace NoErrorsPlugin && add dev eslint check

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -41,15 +41,15 @@ module.exports = {
     },
     module: {
         rules: [
-            // {
-            //     test: /\.(js|vue)$/,
-            //     loader: 'eslint-loader',
-            //     enforce: "pre",
-            //     include: [resolve('src'), resolve('test')],
-            //     options: {
-            //         formatter: require('eslint-friendly-formatter')
-            //     }
-            // },
+            {
+                test: /\.(js|vue)$/,
+                loader: 'eslint-loader',
+                enforce: "pre",
+                include: [resolve('src'), resolve('test')],
+                options: {
+                    formatter: require('eslint-friendly-formatter')
+                }
+            },
             { test: /\.vue$/,
                 loader: 'vue-loader',
                 options: vueLoaderConfig
--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -33,7 +33,7 @@ module.exports = merge(baseWebpackConfig, {
         }),
         // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
         // https://github.com/ampedandwired/html-webpack-plugin
         new HtmlWebpackPlugin({
             filename: 'index.html',
