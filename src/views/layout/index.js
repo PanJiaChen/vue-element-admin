6b85b018032d521c85a5aa0aea54f2d568463695
Haven <baoyx007@gmail.com>
2017-05-15 14:57:26 +0800

    Update index.js
    fix typo

--- src/views/layout/index.js
+++ src/views/layout/index.js
@@ -2,6 +2,6 @@ export { default as Navbar } from './Navbar';
 
 export { default as Sidebar } from './Sidebar';
 
-export { default as Levelbar } from './Sidebar';
+export { default as Levelbar } from './Levelbar';
 
 export { default as AppMain } from './AppMain';
