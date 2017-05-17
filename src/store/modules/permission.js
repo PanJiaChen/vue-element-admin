523a9b3de37c1feb108c16cbee422cd5c27203c7
Pan <panfree23@gmail.com>
2017-05-17 17:53:37 +0800

    add promise

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -24,25 +24,28 @@ const permission = {
   actions: {
     // s
     GenerateRoutes({ commit }, data) {
-      const { roles } = data;
-      const accessedRouters = asyncRouterMap.filter(v => {
-        if (roles.indexOf('admin') >= 0) return true;
-        if (hasPermission(roles, v)) {
-          if (v.children && v.children.length > 0) {
-            v.children = v.children.filter(child => {
-              if (hasPermission(roles, child)) {
-                return child
-              }
-              return false;
-            });
-            return v
-          } else {
-            return v
+      return new Promise(resolve => {
+        const { roles } = data;
+        const accessedRouters = asyncRouterMap.filter(v => {
+          if (roles.indexOf('admin') >= 0) return true;
+          if (hasPermission(roles, v)) {
+            if (v.children && v.children.length > 0) {
+              v.children = v.children.filter(child => {
+                if (hasPermission(roles, child)) {
+                  return child
+                }
+                return false;
+              });
+              return v
+            } else {
+              return v
+            }
           }
-        }
-        return false;
-      });
-      commit('SET_ROUTERS', accessedRouters);
+          return false;
+        });
+        commit('SET_ROUTERS', accessedRouters);
+        resolve();
+      })
     }
   }
 };
