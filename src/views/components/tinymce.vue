737c36d8561bf8cf12cf8673d1b85e9e2d5c6ec7
Cash <ca5h@qq.com>
2017-05-18 14:31:09 +0800

    别字

--- src/views/components/tinymce.vue
+++ src/views/components/tinymce.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="components-container">
-    <code>公司做的后台主要是一个cms系统，公司也是已自媒体为核心的，所以富文本是后台很核心的功能。在选择富文本的过程中也走了不少的弯路，市面上常见的富文本都基本用过了，最终选择了tinymce</code>
+    <code>公司做的后台主要是一个cms系统，公司也是以自媒体为核心的，所以富文本是后台很核心的功能。在选择富文本的过程中也走了不少的弯路，市面上常见的富文本都基本用过了，最终选择了tinymce</code>
     <div class="editor-container">
       <Tinymce :height=200 ref="editor" v-model="content"></Tinymce>
     </div>
