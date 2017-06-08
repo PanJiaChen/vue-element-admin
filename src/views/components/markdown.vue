e9d37a94d36bfea8571ca6147e2f41ee662064a1
Pan <panfree23@gmail.com>
2017-06-08 16:38:25 +0800

    add markdown to html

--- src/views/components/markdown.vue
+++ src/views/components/markdown.vue
@@ -4,6 +4,8 @@
     <div class="editor-container">
       <MdEditor  id='contentEditor' ref="contentEditor" v-model='content' :height="300" :zIndex='20'></MdEditor>
     </div>
+    <el-button @click='markdown2Html' style="margin-top:80px;" type="primary">转为HTML<i class="el-icon-document el-icon--right"></i></el-button>
+    <div v-html="html"></div>
   </div>
 </template>
 <script>
@@ -13,7 +15,16 @@
       components: { MdEditor },
       data() {
         return {
-          content: 'Simplemde'
+          content: 'Simplemde',
+          html: ''
+        }
+      },
+      methods: {
+        markdown2Html() {
+          import('showdown').then(showdown => {
+            const converter = new showdown.Converter();
+            this.html = converter.makeHtml(this.content)
+          })
         }
       }
     };
