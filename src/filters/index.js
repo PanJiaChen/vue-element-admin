7549eb8044cbd1f478bdf17b2408a822ffb9c479
Pan <panfree23@gmail.com>
2017-06-23 15:39:13 +0800

    fix eslint && refine code

--- src/filters/index.js
+++ src/filters/index.js
@@ -104,8 +104,5 @@ export function html2Text(val) {
 
 
 export function toThousandslsFilter(num) {
-  return (+num || 0).toString()
-    .replace(/^\-?\d+/g, function (m) {
-      return m.replace(/(?=(?!\b)(\d{3})+$)/g, ',');
-    });
+  return (+num || 0).toString().replace(/^-?\d+/g, m => m.replace(/(?=(?!\b)(\d{3})+$)/g, ','));
 }
