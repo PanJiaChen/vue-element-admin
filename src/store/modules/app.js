7705a0c79355176bcc2fb06201a088a6aad93124
Pan <panfree23@gmail.com>
2017-07-14 17:37:47 +0800

    refine tab-views

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,6 +1,5 @@
 import Cookies from 'js-cookie';
 
-
 const app = {
   state: {
     sidebar: {
--- src/views/layout/TabsView.vue
+++ src/views/layout/TabsView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='tabs-view-container'>
     <router-link class="tabs-view" v-for="tag in Array.from(visitedViews)" :to="tag.path" :key="tag.path">
-      <el-tag :closable="true" @close='closeViewTabs(tag,$event)'>
+      <el-tag :closable="true" :type="isActive(tag.path)?'primary':''" @close='closeViewTabs(tag,$event)'>
         {{tag.name}}
       </el-tag>
     </router-link>
@@ -29,6 +29,9 @@
       },
       addViewTabs() {
         this.$store.dispatch('addVisitedViews', this.generateRoute())
+      },
+      isActive(path) {
+        return path === this.$route.path
       }
     },
     watch: {
