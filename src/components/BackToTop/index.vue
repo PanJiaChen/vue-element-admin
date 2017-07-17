cbc5c18291098575d603a96b7b802bdede9e9bfd
dongsuo <xuxiaofei1.0@outlook.com>
2017-07-17 18:00:28 +0800

    '代码格式化'

--- src/components/BackToTop/index.vue
+++ src/components/BackToTop/index.vue
@@ -10,102 +10,101 @@
     </div>
   </transition>
 </template>
+
 <script>
-export default {
-  name: 'BackToTop',
-  props: {
-    visibilityHeight: {
-      type: Number,
-      default: 400
-    },
-    backPosition: {
-      type: Number,
-      default: 0
-    },
-    customStyle: {
-      type: Object,
-      default: {
-        right: '50px',
-        bottom: '50px',
-        width: '40px',
-        height: '40px',
-        'border-radius': '4px',
-        'line-height': '40px',
-        background: '#e7eaf1'
+  export default {
+    name: 'BackToTop',
+    props: {
+      visibilityHeight: {
+        type: Number,
+        default: 400
+      },
+      backPosition: {
+        type: Number,
+        default: 0
+      },
+      customStyle: {
+        type: Object,
+        default: {
+          right: '50px',
+          bottom: '50px',
+          width: '40px',
+          height: '40px',
+          'border-radius': '4px',
+          'line-height': '45px',
+          background: '#e7eaf1'
+        }
+      },
+      transitionName: {
+        type: String,
+        default: 'fade'
       }
     },
-    transitionName: {
-      type: String,
-      default: 'fade'
-    }
-
-  },
-  data() {
-    return {
-      visible: false,
-      interval: null
-    }
-  },
-  mounted() {
-    window.addEventListener('scroll', this.handleScroll);
-  },
-  beforeDestroy() {
-    window.removeEventListener('scroll', this.handleScroll);
-    if (this.interval) {
-      clearInterval(this.interval);
-    }
-  },
-  methods: {
-    handleScroll() {
-      this.visible = window.pageYOffset > this.visibilityHeight;
+    data() {
+      return {
+        visible: false,
+        interval: null
+      }
     },
-    backToTop() {
-      const start = window.pageYOffset;
-      let i = 0;
-      this.interval = setInterval(() => {
-        const next = Math.floor(this.easeInOutQuad(10 * i, start, -start, 500));
-        if (next <= this.backPosition) {
-          window.scrollTo(0, this.backPosition);
-          clearInterval(this.interval)
-        } else {
-          window.scrollTo(0, next);
-        }
-        i++;
-      }, 16.7)
+    mounted() {
+      window.addEventListener('scroll', this.handleScroll);
     },
-    easeInOutQuad(t, b, c, d) {
-      if ((t /= d / 2) < 1) return c / 2 * t * t + b;
-      return -c / 2 * (--t * (t - 2) - 1) + b;
+    beforeDestroy() {
+      window.removeEventListener('scroll', this.handleScroll);
+      if (this.interval) {
+        clearInterval(this.interval);
+      }
+    },
+    methods: {
+      handleScroll() {
+        this.visible = window.pageYOffset > this.visibilityHeight;
+      },
+      backToTop() {
+        const start = window.pageYOffset;
+        let i = 0;
+        this.interval = setInterval(() => {
+          const next = Math.floor(this.easeInOutQuad(10 * i, start, -start, 500));
+          if (next <= this.backPosition) {
+            window.scrollTo(0, this.backPosition);
+            clearInterval(this.interval)
+          } else {
+            window.scrollTo(0, next);
+          }
+          i++;
+        }, 16.7)
+      },
+      easeInOutQuad(t, b, c, d) {
+        if ((t /= d / 2) < 1) return c / 2 * t * t + b;
+        return -c / 2 * (--t * (t - 2) - 1) + b;
+      }
     }
   }
-}
 </script>
+
 <style scoped>
-.back-to-top {
-  position: fixed;
-  display: inline-block;
-  text-align: center;
-  cursor: pointer;
-}
+  .back-to-top {
+    position: fixed;
+    display: inline-block;
+    text-align: center;
+    cursor: pointer;
+  }
 
-.back-to-top:hover {
-  background: #d5dbe7;
-}
+  .back-to-top:hover {
+    background: #d5dbe7;
+  }
 
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity .5s;
-}
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity .5s;
+  }
 
-.fade-enter,
-.fade-leave-to {
-  opacity: 0
-}
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0
+  }
 
-.back-to-top .Icon {
-  fill: #9aaabf;
-  background: none;
-}
+  .back-to-top .Icon {
+    fill: #9aaabf;
+    background: none;
+  }
 </style>
-
-
