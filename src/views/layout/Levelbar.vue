77ff823037ea74ef14c18669877cc17260a691ee
Pan <panfree23@gmail.com>
2017-07-26 13:32:02 +0800

    fix levelbar bug

--- src/views/layout/Levelbar.vue
+++ src/views/layout/Levelbar.vue
@@ -2,7 +2,7 @@
   <el-breadcrumb class="app-levelbar" separator="/">
     <el-breadcrumb-item v-for="(item,index)  in levelList" :key="item">
       <router-link v-if='item.redirect==="noredirect"||index==levelList.length-1' to="" class="no-redirect">{{item.name}}</router-link>
-      <router-link v-else :to="item.path">{{item.name}}</router-link>
+      <router-link v-else :to="item.redirect||item.path">{{item.name}}</router-link>
     </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
