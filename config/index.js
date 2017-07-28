8c9ebd73453f63c97a5c583985e412a47fee50c8
Pan <panfree23@gmail.com>
2017-07-28 17:29:07 +0800

    set productionSourceMap: false

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ module.exports = {
         assetsSubDirectory: 'static',
         assetsPublicPath: './',          //请根据自己路径配置更改
         staticPath:'./static/',          //请根据自己路径配置更改
-        productionSourceMap: true,
+        productionSourceMap: false,
         // Gzip off by default as many popular static hosts such as
         // Surge or Netlify already gzip all static assets for you.
         // Before setting to `true`, make sure to:
