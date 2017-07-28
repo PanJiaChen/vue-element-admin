6ad03ac5f06a17a31d462b5022b843bf6be07973
Pan <panfree23@gmail.com>
2017-07-28 16:10:00 +0800

    rm documentImg

--- src/views/errlog/index.vue
+++ src/views/errlog/index.vue
@@ -6,7 +6,7 @@
     <code>
       现在的管理后台基本都是spa的形式了，它增强了用户体验，但同时也会怎增加页面出问题的可能性，可能一个小小的疏忽就导致整个页面的死锁。好在Vue官网提供了一个方法来捕获处理异常
     </code>
-    <a href="#"><img src='../../../documentImg/code1.png'></a>
+    <a href="#"><img src='http://panjiachen.github.io/images/errHandler.png'></a>
   </div>
 </template>
 
