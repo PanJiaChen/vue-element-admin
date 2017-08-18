e912cea26a536070ddc194306fa5441089dfcdd7
Pan <panfree23@gmail.com>
2017-08-18 11:03:07 +0800

    refine sidebaritem code

--- src/views/layout/SidebarItem.vue
+++ src/views/layout/SidebarItem.vue
@@ -1,33 +1,33 @@
 <template>
-	<div class='menu-wrapper'>
-		<template v-for="item in routes">
+  <div class='menu-wrapper'>
+    <template v-for="item in routes">
 
-			<router-link v-if="!item.hidden&&item.noDropdown&&item.children.length>0" :to="item.path+'/'+item.children[0].path">
-				<el-menu-item :index="item.path+'/'+item.children[0].path"  class='submenu-title-noDropdown'>
-					<icon-svg v-if='item.icon' :icon-class="item.icon"></icon-svg><span slot="title">{{item.children[0].name}}</span>
-				</el-menu-item>
-			</router-link>
+      <router-link v-if="!item.hidden&&item.noDropdown&&item.children.length>0" :to="item.path+'/'+item.children[0].path">
+        <el-menu-item :index="item.path+'/'+item.children[0].path" class='submenu-title-noDropdown'>
+          <icon-svg v-if='item.icon' :icon-class="item.icon"></icon-svg><span>{{item.children[0].name}}</span>
+        </el-menu-item>
+      </router-link>
 
-			<el-submenu :index="item.name" v-if="!item.noDropdown&&!item.hidden">
-				<template slot="title">
-					<icon-svg v-if='item.icon' :icon-class="item.icon"></icon-svg><span>{{item.name}}</span>
-				</template>
-				<template v-for="child in item.children" v-if='!child.hidden'>
+      <el-submenu :index="item.name" v-if="!item.noDropdown&&!item.hidden">
+        <template slot="title">
+          <icon-svg v-if='item.icon' :icon-class="item.icon"></icon-svg><span>{{item.name}}</span>
+        </template>
+        <template v-for="child in item.children" v-if='!child.hidden'>
 
-					<sidebar-item class='nest-menu' v-if='child.children&&child.children.length>0' :routes='[child]'> </sidebar-item>
+          <sidebar-item class='nest-menu' v-if='child.children&&child.children.length>0' :routes='[child]'> </sidebar-item>
 
-					<router-link v-else :to="item.path+'/'+child.path">
-						<el-menu-item :index="item.path+'/'+child.path">
-								<icon-svg v-if='child.icon' :icon-class="child.icon"></icon-svg><span>{{child.name}}</span>
-						</el-menu-item>
-					</router-link>
+          <router-link v-else :to="item.path+'/'+child.path">
+            <el-menu-item :index="item.path+'/'+child.path">
+              <icon-svg v-if='child.icon' :icon-class="child.icon"></icon-svg><span>{{child.name}}</span>
+            </el-menu-item>
+          </router-link>
 
-				</template>
+        </template>
 
-			</el-submenu>
+      </el-submenu>
 
-		</template>
-	</div>
+    </template>
+  </div>
 </template>
 
 <script>
@@ -40,8 +40,4 @@
     }
   }
 </script>
-
-<style rel="stylesheet/scss" lang="scss" scoped>
-
-</style>
 
