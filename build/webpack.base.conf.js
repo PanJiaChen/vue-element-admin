97570fa6123687d930e2ca48d8a3fce5b0ced020
Pan <panfree23@gmail.com>
2017-08-29 16:28:28 +0800

    refactor icons

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -57,8 +57,17 @@ module.exports = {
         include: [resolve('src'), resolve('test')]
       },
       {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+        include: [resolve('src/icons')],
+        options: {
+          symbolId: 'icon-[name]'
+        }
+      },
+      {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
+        exclude: [resolve('src/icons')],
         query: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
--- src/main.js
+++ src/main.js
@@ -4,15 +4,12 @@ import 'element-ui/lib/theme-default/index.css'
 import App from './App'
 import router from './router'
 import store from './store'
-import '@/assets/iconfont/iconfont' // iconfont 具体图标见wiki
-import IconSvg from '@/components/Icon-svg'// svg组件
 import * as filters from '@/filters' // 全局filter
+import '@/icons' // icon
 import '@/errorLog'// error log
 import '@/permission' // 权限
-import '@/mock/index.js'  // 该项目所有请求使用mockjs模拟
+import '@/mock'  // 该项目所有请求使用mockjs模拟
 
-// register globally
-Vue.component('icon-svg', IconSvg)
 Vue.use(ElementUI)
 
 // register global utility filters.
