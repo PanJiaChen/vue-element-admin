f984fe8d522280a953f805c11b343b8354b2d0d8
Hex <hex@codeigniter.org.cn>
2017-09-08 15:37:13 +0800

    与 vue-cli webpack 模板不一致

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -68,7 +68,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         exclude: [resolve('src/icons')],
-        query: {
+        options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
@@ -76,7 +76,7 @@ module.exports = {
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
-        query: {
+        options: {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
