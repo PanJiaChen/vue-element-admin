7b51ffe11636d349fb071ebcfdd24211a0b51ced
Pan <panfree23@gmail.com>
2017-09-29 13:52:29 +0800

    fix build_config bug

--- config/index.js
+++ config/index.js
@@ -3,37 +3,37 @@ var path = require('path')
 
 module.exports = {
     build: {
-        sitEnv: require('./sit.env'),
-        prodEnv: require('./prod.env'),
-        index: path.resolve(__dirname, '../dist/index.html'),
-        assetsRoot: path.resolve(__dirname, '../dist'),
-        assetsSubDirectory: 'static',
-        assetsPublicPath: './',          //请根据自己路径配置更改
-        productionSourceMap: false,
-        // Gzip off by default as many popular static hosts such as
-        // Surge or Netlify already gzip all static assets for you.
-        // Before setting to `true`, make sure to:
-        // npm install --save-dev compression-webpack-plugin
-        productionGzip: false,
-        productionGzipExtensions: ['js', 'css'],
-        // Run the build command with an extra argument to
-        // View the bundle analyzer report after build finishes:
-        // `npm run build --report`
-        // Set to `true` or `false` to always turn it on or off
-        bundleAnalyzerReport: process.env.npm_config_report
+			sitEnv: require('./sit.env'),
+			prodEnv: require('./prod.env'),
+			index: path.resolve(__dirname, '../dist/index.html'),
+			assetsRoot: path.resolve(__dirname, '../dist'),
+			assetsSubDirectory: 'static',
+			assetsPublicPath: './',          //请根据自己路径配置更改
+			productionSourceMap: false,
+			// Gzip off by default as many popular static hosts such as
+			// Surge or Netlify already gzip all static assets for you.
+			// Before setting to `true`, make sure to:
+			// npm install --save-dev compression-webpack-plugin
+			productionGzip: false,
+			productionGzipExtensions: ['js', 'css'],
+			// Run the build command with an extra argument to
+			// View the bundle analyzer report after build finishes:
+			// `npm run build --report`
+			// Set to `true` or `false` to always turn it on or off
+			bundleAnalyzerReport: process.env.npm_config_report
     },
     dev: {
-        env: require('./dev.env'),
-        port: 9527,
-        autoOpenBrowser: true,
-        assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
-        proxyTable: {},
-        // CSS Sourcemaps off by default because relative paths are "buggy"
-        // with this option, according to the CSS-Loader README
-        // (https://github.com/webpack/css-loader#sourcemaps)
-        // In our experience, they generally work as expected,
-        // just be aware of this issue when enabling this option.
-        cssSourceMap: false
+			env: require('./dev.env'),
+			port: 9527,
+			autoOpenBrowser: true,
+			assetsSubDirectory: 'static',
+			assetsPublicPath: '/',
+			proxyTable: {},
+			// CSS Sourcemaps off by default because relative paths are "buggy"
+			// with this option, according to the CSS-Loader README
+			// (https://github.com/webpack/css-loader#sourcemaps)
+			// In our experience, they generally work as expected,
+			// just be aware of this issue when enabling this option.
+			cssSourceMap: false
     }
 }
