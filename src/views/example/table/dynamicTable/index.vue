577e4dc17e6b3c4e77af7ba310e40d28d9b0a619
Pan <panfree23@gmail.com>
2017-10-26 14:21:21 +0800

    refactor: case sensitive

---
+++ src/views/example/table/dynamicTable/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-container">
+    <div style='margin:0 0 5px 20px'>固定表头 按照表头顺序排序</div>
+    <fixed-thead></fixed-thead>
+
+    <div style='margin:30px 0 5px 20px'>不固定表头 按照点击顺序排序</div>
+    <unfixed-thead></unfixed-thead>
+  </div>
+</template>
+
+<script>
+import fixedThead from './fixedThead'
+import unfixedThead from './unfixedThead'
+
+export default {
+  components: { fixedThead, unfixedThead }
+}
+</script>
+
