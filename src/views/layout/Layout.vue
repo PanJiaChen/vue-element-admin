c53fb7fbb12d8f9fdbb2b3662ca113311dfd8d70
Pan <panfree23@gmail.com>
2017-11-02 15:51:29 +0800

    style:refine sidebar css

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -31,35 +31,9 @@ export default {
 <style rel="stylesheet/scss" lang="scss" scoped>
 	@import "src/styles/mixin.scss";
 	.app-wrapper {
-		@include clearfix;
-		position: relative;
-		height: 100%;
-		width: 100%;
-		&.hideSidebar {
-			.sidebar-container{
-				width:36px;
-				overflow: inherit;
-			}
-			.main-container {
-				margin-left: 36px;
-			}
-		}
-		.sidebar-container {
-			transition: width 0.28s ease-out;
-			width: 180px;
-			height: 100%;
-			position: fixed;
-			top: 0;
-			bottom: 0;
-			left: 0;
-			z-index: 1001;
-			overflow-y: auto;
- 			&::-webkit-scrollbar {display:none}
-		}
-		.main-container {
-			min-height: 100%;
-			transition: margin-left 0.28s ease-out;
-			margin-left: 180px;
-		}
+	  @include clearfix;
+	  position: relative;
+	  height: 100%;
+	  width: 100%;
 	}
 </style>
