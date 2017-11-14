35055c5e511be25a871f7700baeaa686861ff693
Pan <panfree23@gmail.com>
2017-11-14 10:09:32 +0800

    style:breadcrumb add transition

--- src/views/layout/components/Levelbar.vue
+++ src/views/layout/components/Levelbar.vue
@@ -1,9 +1,11 @@
 <template>
   <el-breadcrumb class="app-levelbar" separator="/">
+   <transition-group name="breadcrumb">
     <el-breadcrumb-item v-for="(item,index)  in levelList" :key="item.path" v-if='item.meta.title'>
       <span v-if='item.redirect==="noredirect"||index==levelList.length-1' class="no-redirect">{{generateTitle(item.meta.title)}}</span>
       <router-link v-else :to="item.redirect||item.path">{{generateTitle(item.meta.title)}}</router-link>
     </el-breadcrumb-item>
+     </transition-group>
   </el-breadcrumb>
 </template>
 
