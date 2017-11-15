923fc72b0f448c3a9b0dd51535db74b64290b48d
Reza Haidari .A <shoosh2007@gmail.com>
2017-11-15 10:57:19 +0330

    add validate email method

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -31,3 +31,13 @@ export function validatAlphabets(str) {
   return reg.test(str)
 }
 
+/**
+ * validate email
+ * @param email
+ * @returns {boolean}
+ */
+export function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
