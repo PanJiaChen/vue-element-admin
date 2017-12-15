4d8017d0377834afe7dd0ee04d1bc7b9471a1311
Pan <panfree23@gmail.com>
2017-12-15 10:58:33 +0800

    perf:settings

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,6 +3,7 @@
 module.exports = {
   "plugins": {
     // to edit target browsers: use "browserslist" field in package.json
+    "postcss-import": {},
     "autoprefixer": {}
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,3 @@
     name: 'APP'
   }
 </script>
-
-<style lang="scss">
-@import '~normalize.css/normalize.css'; // normalize.css 样式格式化
-@import './styles/index.scss'; // 全局自定义样式
-</style>
