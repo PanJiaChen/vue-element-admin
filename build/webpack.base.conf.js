021c147b3116ade2b2a50ed6144224df985e8465
Pan <panfree23@gmail.com>
2017-12-18 10:19:35 +0800

    perf:code

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -49,7 +49,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader?cacheDirectory',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'),resolve('node_modules/webpack-dev-server/client')]]
       },
       {
         test: /\.svg$/,
--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -161,6 +161,3 @@ export default {
   }
 }
 </style>
-
-
-
