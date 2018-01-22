d97772595a77776056c0aa95d19347ad2fe7cb7b
lei.jiang <jianglei19940909@126.com>
2018-01-22 13:04:00 +0800

    修复marLTemp变量未清空的bug

--- src/views/example/table/treeTable/customEval.js
+++ src/views/example/table/treeTable/customEval.js
@@ -4,7 +4,8 @@
 */
 'use strict'
 import Vue from 'vue'
-export default function treeToArray(data, parent, level, expandedAll, item, marLTemp = []) {
+export default function treeToArray(data, parent, level, expandedAll, item) {
+  const marLTemp = []
   let tmp = []
   Array.from(data).forEach(function(record) {
     if (record._expanded === undefined) {
