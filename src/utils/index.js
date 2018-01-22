3bed61fd073d9e4374db2ae79044dbb8cd90b052
Serge Gao <gaosj1993@foxmail.com>
2018-01-22 10:34:21 +0800

    refactoring deepClone

--- src/utils/index.js
+++ src/utils/index.js
@@ -249,19 +249,6 @@ export function debounce(func, wait, immediate) {
 }
 
 export function deepClone(source) {
-  if (!source && typeof source !== 'object') {
-    throw new Error('error arguments', 'shallowClone')
-  }
-  const targetObj = source.constructor === Array ? [] : {}
-  for (const keys in source) {
-    if (source.hasOwnProperty(keys)) {
-      if (source[keys] && typeof source[keys] === 'object') {
-        targetObj[keys] = source[keys].constructor === Array ? [] : {}
-        targetObj[keys] = deepClone(source[keys])
-      } else {
-        targetObj[keys] = source[keys]
-      }
-    }
-  }
+  let targetObj = JSON.parse(JSON.stringify(source));
   return targetObj
 }
