840eda6e2797d7f4ed0f1a52d2a346ddb5f7953f
Pan <panfree23@gmail.com>
2018-01-22 15:55:40 +0800

    fix[login]: fixed thirtparty login dialog bug

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -36,7 +36,7 @@
       <el-button class="thirdparty-button" type="primary" @click="showDialog=true">{{$t('login.thirdparty')}}</el-button>
     </el-form>
 
-    <el-dialog :title="$t('login.thirdparty')" :visible.sync="showDialog">
+    <el-dialog :title="$t('login.thirdparty')" :visible.sync="showDialog" append-to-body>
       {{$t('login.thirdpartyTips')}}
       <br/>
       <br/>
