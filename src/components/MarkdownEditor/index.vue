d754eae662971a2caf0e633d30f36211e8084288
Pan <panfree23@gmail.com>
2018-01-29 14:45:43 +0800

    perf[markdown-editor]

--- src/components/MarkdownEditor/index.vue
+++ src/components/MarkdownEditor/index.vue
@@ -73,47 +73,44 @@ export default {
     })
   },
   destroyed() {
+    this.simplemde.toTextArea()
     this.simplemde = null
   }
 }
 </script>
 
-<style>
-.simplemde-container .CodeMirror {
+<style scoped>
+.simplemde-container>>>.CodeMirror {
+  min-height: 150px;
+  line-height: 20px;
+}
+
+.simplemde-container>>>.CodeMirror-scroll {
   min-height: 150px;
 }
 
-.simplemde-container .CodeMirror-scroll {
-  min-height: 150px;
-}
-
-.simplemde-container .CodeMirror-code {
+.simplemde-container>>>.CodeMirror-code {
   padding-bottom: 40px;
 }
 
-.simplemde-container .editor-statusbar {
+.simplemde-container>>>.editor-statusbar {
   display: none;
 }
 
-.simplemde-container .CodeMirror .CodeMirror-code .cm-link {
-  color: #1482F0;
+.simplemde-container>>>.CodeMirror .CodeMirror-code .cm-link {
+  color: #1890ff;
 }
 
-.simplemde-container .CodeMirror .CodeMirror-code .cm-string.cm-url {
+.simplemde-container>>>.CodeMirror .CodeMirror-code .cm-string.cm-url {
   color: #2d3b4d;
-  font-weight: bold;
 }
 
-.simplemde-container .CodeMirror .CodeMirror-code .cm-formatting-link-string.cm-url {
+.simplemde-container>>>.CodeMirror .CodeMirror-code .cm-formatting-link-string.cm-url {
   padding: 0 2px;
-  font-weight: bold;
   color: #E61E1E;
 }
-
-.simplemde-container .editor-toolbar.fullscreen,
-.simplemde-container .CodeMirror-fullscreen {
+.simplemde-container >>> .editor-toolbar.fullscreen,
+.simplemde-container >>> .CodeMirror-fullscreen {
   z-index: 1003;
 }
 </style>
-
-
