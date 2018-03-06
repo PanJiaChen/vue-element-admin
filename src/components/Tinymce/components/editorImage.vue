31b7fa6f5503fdb9d8a89502f1238d02386340cd
Pan <panfree23@gmail.com>
2018-03-06 17:09:09 +0800

    perf[Tinymce]: set dialog append-to-body

--- src/components/Tinymce/components/editorImage.vue
+++ src/components/Tinymce/components/editorImage.vue
@@ -2,7 +2,7 @@
   <div class="upload-container">
     <el-button icon='el-icon-upload' size="mini" :style="{background:color,borderColor:color}" @click=" dialogVisible=true" type="primary">上传图片
     </el-button>
-    <el-dialog :visible.sync="dialogVisible">
+    <el-dialog append-to-body :visible.sync="dialogVisible">
       <el-upload class="editor-slide-upload" action="https://httpbin.org/post" :multiple="true" :file-list="fileList" :show-file-list="true"
         list-type="picture-card" :on-remove="handleRemove" :on-success="handleSuccess" :before-upload="beforeUpload">
         <el-button size="small" type="primary">点击上传</el-button>
