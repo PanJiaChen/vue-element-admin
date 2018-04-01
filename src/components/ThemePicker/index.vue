385c021651b72478700d9919434657116574c7ea
WangXinhai <18907191365@189.cn>
2018-04-01 15:45:55 +0800

    fix

--- src/components/ThemePicker/index.vue
+++ src/components/ThemePicker/index.vue
@@ -69,7 +69,8 @@ export default {
       const colorOverrides = [] // only capture color overides
       oldCluster.forEach((color, index) => {
         const value = newCluster[index]
-        const repl = new RegExp(`(^|})([^{]+{[^{}]+)${color}\\b([^}]*)(?=})`, 'gi')
+        const color_plain = color.replace(/([()])/g,'\\$1')
+        const repl = new RegExp(`(^|})([^{]+{[^{}]+)${color_plain}\\b([^}]*)(?=})`, 'gi')
         const nestRepl = new RegExp(color, 'ig') // for greed matching before the 'color'
         let v
         while ((v = repl.exec(style))) {
