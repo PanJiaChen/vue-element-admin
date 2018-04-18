cc5e62d5f50231b89fec65c1d3ce1b82b5b1b8ef
Pan <panfree23@gmail.com>
2018-04-18 17:00:43 +0800

    fix[contextmenu]: adjust contextmenu z-index

--- src/views/layout/components/TagsView.vue
+++ src/views/layout/components/TagsView.vue
@@ -156,7 +156,7 @@ export default {
   .contextmenu {
     margin: 0;
     background: #fff;
-    z-index: 2;
+    z-index: 100;
     position: absolute;
     list-style-type: none;
     padding: 5px 0;
