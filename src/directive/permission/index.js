59dfd0f34e2ffcd15df30e2cae6b15259cff1c1d
Pan <panfree23@gmail.com>
2018-05-08 22:12:55 +0800

    feature[permission]: add v-permission

---
+++ src/directive/permission/index.js
@@ -0,0 +1,13 @@
+import permission from './permission'
+
+const install = function(Vue) {
+  Vue.directive('permission', permission)
+}
+
+if (window.Vue) {
+  window['permission'] = permission
+  Vue.use(install); // eslint-disable-line
+}
+
+permission.install = install
+export default permission
