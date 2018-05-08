597df4844a8e84aa9049512b3c229a5613e18285
花裤衩 <panfree23@gmail.com>
2018-05-08 22:15:34 +0800

    feature[permission]: add v-permission (#653)

--- src/lang/zh.js
+++ src/lang/zh.js
@@ -4,6 +4,8 @@ export default {
     introduction: '简述',
     documentation: '文档',
     permission: '权限测试页',
+    pagePermission: '页面权限',
+    directivePermission: '指令权限',
     icons: '图标',
     components: '组件',
     componentIndex: '介绍',
--- src/router/index.js
+++ src/router/index.js
@@ -67,15 +67,27 @@ export const asyncRouterMap = [
     path: '/permission',
     component: Layout,
     redirect: '/permission/index',
-    meta: { roles: ['admin'] }, // you can set roles in root nav
+    alwaysShow: true, // will always show the root menu
+    meta: {
+      title: 'permission',
+      icon: 'lock',
+      roles: ['admin', 'editor'] // you can set roles in root nav
+    },
     children: [{
-      path: 'index',
-      component: _import('permission/index'),
-      name: 'permission',
+      path: 'page',
+      component: _import('permission/page'),
+      name: 'pagePermission',
       meta: {
-        title: 'permission',
-        icon: 'lock',
+        title: 'pagePermission',
         roles: ['admin'] // or you can only set roles in sub nav
+      }
+    }, {
+      path: 'directive',
+      component: _import('permission/directive'),
+      name: 'directivePermission',
+      meta: {
+        title: 'directivePermission'
+        // if do not set roles, means: this page does not require permission
       }
     }]
   },
