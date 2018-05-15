ce3fb856c407775d290f0d2d0c6d90343125d038
gooking <javazj@qq.com>
2018-05-15 18:51:48 +0800

    fix bug

---
+++ .eslintignore
@@ -0,0 +1,3 @@
+build/*.js
+config/*.js
+src/assets
---
+++ .postcssrc.js
@@ -0,0 +1,10 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  "plugins": {
+    "postcss-import": {},
+    "postcss-url": {},
+    // to edit target browsers: use "browserslist" field in package.json
+    "autoprefixer": {}
+  }
+}
