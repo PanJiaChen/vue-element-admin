c861dd10cf392a4f9231def8cdc6f993e6136758
Pan <panfree23@gmail.com>
2018-05-28 15:33:47 +0800

    fix: [404.vue]: return-home button href bug

--- src/views/errorPage/404.vue
+++ src/views/errorPage/404.vue
@@ -14,7 +14,7 @@
         </div>
         <div class="bullshit__headline">{{ message }}</div>
         <div class="bullshit__info">请检查您输入的网址是否正确，请点击以下按钮返回主页或者发送错误报告</div>
-        <a href="/" class="bullshit__return-home">返回首页</a>
+        <a href="" class="bullshit__return-home">返回首页</a>
       </div>
     </div>
   </div>
