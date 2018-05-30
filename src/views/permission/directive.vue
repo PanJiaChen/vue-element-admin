20aad464165552c49909cd855a8ae51a72de564e
Pan <panfree23@gmail.com>
2018-05-30 15:27:44 +0800

    fix[permission]: directive demo typo

--- src/views/permission/directive.vue
+++ src/views/permission/directive.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="app-container">
     <switch-roles @change="handleRolesChange" />
-
     <div :key="key" style="margin-top:30px;">
       <span v-permission="['admin']" class="permission-alert">
-        Only <el-tag class="permission-tag" size="small">admin</el-tag> can see this
+        Only
+        <el-tag class="permission-tag" size="small">admin</el-tag> can see this
       </span>
       <span v-permission="['editor']" class="permission-alert">
-        Only <el-tag class="permission-tag" size="small">editor</el-tag> can see this
+        Only
+        <el-tag class="permission-tag" size="small">editor</el-tag> can see this
       </span>
-      <span v-permission="[]" class="permission-alert">
-        Both <el-tag class="permission-tag" size="small">admin</el-tag> and <el-tag class="permission-tag" size="small">editor</el-tag> can see this
+      <span v-permission="['admin','editor']" class="permission-alert">
+        Both
+        <el-tag class="permission-tag" size="small">admin</el-tag> and
+        <el-tag class="permission-tag" size="small">editor</el-tag> can see this
       </span>
     </div>
   </div>
