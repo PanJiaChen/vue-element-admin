0a1c9203d17a9d878f38df9dcaad474c2b9276fb
insua <insua@sohu.com>
2018-06-01 14:28:45 +0800

    fix(Tinymce): When content is null,set content to '' to avoid error

--- src/components/Tinymce/index.vue
+++ src/components/Tinymce/index.vue
@@ -50,6 +50,7 @@ export default {
   watch: {
     value(val) {
       if (!this.hasChange && this.hasInit) {
+        if(val === null) val = ''
         this.$nextTick(() => window.tinymce.get(this.tinymceId).setContent(val))
       }
     }
