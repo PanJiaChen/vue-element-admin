b21b38e2110c3901a7152802406e97ef2a7c8dc4
tmpbook <nauy2011@126.com>
2018-06-12 12:17:17 +0800

    Support route query in TagView layout

--- src/views/layout/components/TagsView.vue
+++ src/views/layout/components/TagsView.vue
@@ -2,7 +2,7 @@
   <div class="tags-view-container">
     <scroll-pane class='tags-view-wrapper' ref='scrollPane'>
       <router-link ref='tag' class="tags-view-item" :class="isActive(tag)?'active':''" v-for="tag in Array.from(visitedViews)"
-        :to="tag.path" :key="tag.path" @contextmenu.prevent.native="openMenu(tag,$event)">
+        :to="tag" :key="tag.path" @contextmenu.prevent.native="openMenu(tag,$event)">
         {{generateTitle(tag.title)}}
         <span class='el-icon-close' @click.prevent.stop='closeSelectedTag(tag)'></span>
       </router-link>
