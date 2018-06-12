2a590a208793810dc7f69ed74d2d90c418f43f4e
临书 <nauy2011@126.com>
2018-06-12 13:53:56 +0800

    Support route query in TagView (#765)

--- src/store/modules/tagsView.js
+++ src/store/modules/tagsView.js
@@ -6,11 +6,9 @@ const tagsView = {
   mutations: {
     ADD_VISITED_VIEWS: (state, view) => {
       if (state.visitedViews.some(v => v.path === view.path)) return
-      state.visitedViews.push({
-        name: view.name,
-        path: view.path,
+      state.visitedViews.push(Object.assign({}, view, {
         title: view.meta.title || 'no-name'
-      })
+      }))
       if (!view.meta.noCache) {
         state.cachedViews.push(view.name)
       }
