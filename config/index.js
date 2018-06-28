5070e20dea0ed659df20d2610d70ef8115d0cefd
Pan <panfree23@gmail.com>
2018-06-28 13:20:44 +0800

    update docs

--- config/index.js
+++ config/index.js
@@ -13,7 +13,10 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+
+    // can be overwritten by process.env.HOST
+    // if you want dev by ip, please set host: '0.0.0.0'
+    host: 'localhost',
     port: 9527, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
