700cc822cd34f762e80280b0391c38419032fbfb
Pan <panfree23@gmail.com>
2018-07-12 12:44:53 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build:prod
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:PanJiaChen/vue-element-admin.git master:gh-pages
+
+cd -
