27012a4b5286aaeb608269632afc6d7f0b490ae0
Pan <panfree23@gmail.com>
2018-07-13 11:28:06 +0800

    touch .nojekyll

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,9 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+#创建.nojekyll 防止Github Pages build错误
+touch .nojekyll
+
 git init
 git add -A
 git commit -m 'deploy'
