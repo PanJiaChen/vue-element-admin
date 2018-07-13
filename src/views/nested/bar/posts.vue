aa7eab58f9337b72684a0049cb96ec1f10c73557
博文 <z.bowen66@gmail.com>
2018-07-13 11:23:06 +0800

    refactor(SidebarItem): optimizate SidebarItem (#845)
    * refactor(sidebar-item): optimizate SiderbarItem.
    
    * chore: update nested examples.
    
    * fix: fix a wrong path.
    
    * fix: fix a transition bug.
    
    * fix: fix a wrong path.
    
    * perf: using "router" mode of el-menu.
    
    * Revert "perf: using "router" mode of el-menu."
    
    This reverts commit cef02a30b02a31242521e9e678cfeaad6f09448c.
    
    * fix: complement i18n text.

--- src/views/nested/bar/posts.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div style="margin-top:30px;">
-    <el-alert title="Children: Posts" type="warning" :closable="false">
-    </el-alert>
-  </div>
-</template>
--- src/views/nested/bar/profile.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div style="margin-top:30px;">
-    <el-alert title="Children: Profile" type="success" :closable="false">
-    </el-alert>
-  </div>
-</template>
