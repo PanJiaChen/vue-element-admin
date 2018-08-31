2fb26525faf0f0c68f6f034d4e4ba01a019702b5
花裤衩 <panfree23@gmail.com>
2018-08-31 16:11:04 +0800

    feature[tagsView]: add refresh  (#1033)
    * add refresh
    
    * fix bug

--- src/lang/zh.js
+++ src/lang/zh.js
@@ -147,6 +147,7 @@ export default {
     tips: 'Tips: 它区别于 navbar 上的 theme-pick, 是两种不同的换肤方法，各自有不同的应用场景，具体请参考文档。'
   },
   tagsView: {
+    refresh: '刷新',
     close: '关闭',
     closeOthers: '关闭其它',
     closeAll: '关闭所有'
