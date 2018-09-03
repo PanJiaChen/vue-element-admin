60540aa5e4ad0aa6b9730ac0b8d23f52b085c3c5
Pan <panfree23@gmail.com>
2018-09-03 16:36:55 +0800

    add redirect

--- src/permission.js
+++ src/permission.js
@@ -52,7 +52,7 @@ router.beforeEach((to, from, next) => {
     if (whiteList.indexOf(to.path) !== -1) { // 在免登录白名单，直接进入
       next()
     } else {
-      next('/login') // 否则全部重定向到登录页
+      next(`/login?redirect=${to.path}`) // 否则全部重定向到登录页
       NProgress.done() // if current page is login will not trigger afterEach hook, so manually handle it
     }
   }
