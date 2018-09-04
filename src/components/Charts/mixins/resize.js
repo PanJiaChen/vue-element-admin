44e4bddc21172854d35c6740c3d995363ce716bd
Pan <panfree23@gmail.com>
2018-09-04 14:54:40 +0800

    perf[Charts]: refine resize-mixin

--- src/components/Charts/mixins/resize.js
+++ src/components/Charts/mixins/resize.js
@@ -8,8 +8,21 @@ export default {
       }
     }, 100)
     window.addEventListener('resize', this.__resizeHandler)
+
+    const sidebarElm = document.getElementsByClassName('sidebar-container')[0]
+    sidebarElm.addEventListener('transitionend', this.sidebarResizeHandler)
   },
   beforeDestroy() {
     window.removeEventListener('resize', this.__resizeHandler)
+
+    const sidebarElm = document.getElementsByClassName('sidebar-container')[0]
+    sidebarElm.removeEventListener('transitionend', this.sidebarResizeHandler)
+  },
+  methods: {
+    sidebarResizeHandler(e) {
+      if (e.propertyName === 'width') {
+        this.__resizeHandler()
+      }
+    }
   }
 }
