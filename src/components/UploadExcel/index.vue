9a2946b00acc71702fad4cbce775858e3be2f22c
zhaoguoweiLLHC <2644572487@qq.com>
2018-09-27 12:53:20 +0800

    Update index.vue

--- src/components/UploadExcel/index.vue
+++ src/components/UploadExcel/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <input ref="excel-upload-input" class="excel-upload-input"  type="file" accept=".xlsx, .xls" @change="handleClick">
+    <input ref="excel-upload-input" class="excel-upload-input" type="file" accept=".xlsx, .xls" @change="handleClick">
     <div class="drop" @drop="handleDrop" @dragover="handleDragover" @dragenter="handleDragover">
       Drop excel file here or
       <el-button :loading="loading" style="margin-left:16px;" size="mini" type="primary" @click="handleUpload">Browse</el-button>
