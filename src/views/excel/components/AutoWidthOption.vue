f56385788c6bea3133cdd04469dde2d553d1c41e
花裤衩 <panfree23@gmail.com>
2018-09-29 13:23:00 +0800

    feature[Excel]: support bookType option (#1144)
    Documentation: https://panjiachen.github.io/vue-element-admin-site/feature/component/excel.html

---
+++ src/views/excel/components/AutoWidthOption.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="display:inline-block;">
+    <label class="radio-label">Cell Auto-Width: </label>
+    <el-radio-group v-model="autoWidth">
+      <el-radio :label="true" border>True</el-radio>
+      <el-radio :label="false" border>False</el-radio>
+    </el-radio-group>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      default: true
+    }
+  },
+  computed: {
+    autoWidth: {
+      get() {
+        return this.value
+      },
+      set(val) {
+        this.$emit('input', val)
+      }
+    }
+  }
+}
+</script>
--- src/views/excel/exportExcel.vue
+++ src/views/excel/exportExcel.vue
@@ -2,14 +2,15 @@
   <!-- $t is vue-i18n global function to translate lang -->
   <div class="app-container">
 
-    <label class="radio-label" style="padding-left:0;">Filename: </label>
-    <el-input :placeholder="$t('excel.placeholder')" v-model="filename" style="width:340px;" prefix-icon="el-icon-document"/>
-    <label class="radio-label">Cell Auto-Width: </label>
-    <el-radio-group v-model="autoWidth">
-      <el-radio :label="true" border>True</el-radio>
-      <el-radio :label="false" border>False</el-radio>
-    </el-radio-group>
-    <el-button :loading="downloadLoading" style="margin:0 0 20px 20px;" type="primary" icon="document" @click="handleDownload">{{ $t('excel.export') }} Excel</el-button>
+    <div>
+      <FilenameOption v-model="filename" />
+      <AutoWidthOption v-model="autoWidth" />
+      <BookTypeOption v-model="bookType" />
+      <el-button :loading="downloadLoading" style="margin:0 0 20px 20px;" type="primary" icon="document" @click="handleDownload">{{ $t('excel.export') }} Excel</el-button>
+      <a href="https://panjiachen.github.io/vue-element-admin-site/feature/component/excel.html" target="_blank" style="margin-left:15px;">
+        <el-tag type="info">Documentation</el-tag>
+      </a>
+    </div>
 
     <el-table v-loading="listLoading" :data="list" element-loading-text="拼命加载中" border fit highlight-current-row>
       <el-table-column align="center" label="Id" width="95">
@@ -46,15 +47,22 @@
 import { fetchList } from '@/api/article'
 import { parseTime } from '@/utils'
 
+// options components
+import FilenameOption from './components/FilenameOption'
+import AutoWidthOption from './components/AutoWidthOption'
+import BookTypeOption from './components/BookTypeOption'
+
 export default {
   name: 'ExportExcel',
+  components: { FilenameOption, AutoWidthOption, BookTypeOption },
   data() {
     return {
       list: null,
       listLoading: true,
       downloadLoading: false,
       filename: '',
-      autoWidth: true
+      autoWidth: true,
+      bookType: 'xlsx'
     }
   },
   created() {
@@ -79,7 +87,8 @@ export default {
           header: tHeader,
           data,
           filename: this.filename,
-          autoWidth: this.autoWidth
+          autoWidth: this.autoWidth,
+          bookType: this.bookType
         })
         this.downloadLoading = false
       })
