711d67c1d3ed633b497e24986247e3d73f9c477c
Pan <panfree23@gmail.com>
2018-10-15 13:11:07 +0800

    tweak

--- src/mock/index.js
+++ src/mock/index.js
@@ -4,9 +4,10 @@ import articleAPI from './article'
 import remoteSearchAPI from './remoteSearch'
 import transactionAPI from './transaction'
 
-// * 修复 Mock 情况下，设置 withCredentials = true，且未被拦截的跨域请求丢失 Cookies 的问题
+// 修复在使用 MockJS 情况下，设置 withCredentials = true，且未被拦截的跨域请求丢失 Cookies 的问题
+// https://github.com/nuysoft/Mock/issues/300
 Mock.XHR.prototype.proxy_send = Mock.XHR.prototype.send
-Mock.XHR.prototype.send = function () {
+Mock.XHR.prototype.send = function() {
   if (this.custom.xhr) {
     this.custom.xhr.withCredentials = this.withCredentials || false
   }
