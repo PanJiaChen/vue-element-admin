112823227cf09beddf3ff62c350de49d2124a6c8
zthxxx <zthxxx.me@gmail.com>
2018-10-20 01:58:26 +0800

    feat: perfect migrate to @vue/cli-service, upgrade vue babel version
    1. update to @vue/cli-service@3.0.5, @babel/core@7.0.0
    2. use vue-cli service replace config file in build/ and config/
    3. upgrade vue and babel configuration
    4. solve the svg-sprite config problem #980
    
    refs: #932 #1087  #980 #1056

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: process.env.BASE_API, // api 的 base_url
+  baseURL: process.env.VUE_APP_BASE_API, // api 的 base_url
   timeout: 5000 // request timeout
 })
 
