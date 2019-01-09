9cea3c3abe31746c044db1388d24ee86e343a623
Pan <panfree23@gmail.com>
2019-01-09 16:04:24 +0800

    refine: GetUserInfo error message

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -63,15 +63,16 @@ const user = {
     GetUserInfo({ commit, state }) {
       return new Promise((resolve, reject) => {
         getUserInfo(state.token).then(response => {
-          if (!response.data) { // 由于mockjs 不支持自定义状态码只能这样hack
-            reject('error')
+          // 由于mockjs 不支持自定义状态码只能这样hack
+          if (!response.data) {
+            reject('Verification failed, please login again.')
           }
           const data = response.data
 
           if (data.roles && data.roles.length > 0) { // 验证返回的roles是否是一个非空数组
             commit('SET_ROLES', data.roles)
           } else {
-            reject('getInfo: roles must be a non-null array !')
+            reject('getInfo: roles must be a non-null array!')
           }
 
           commit('SET_NAME', data.name)
