22cdfc246c749dbf8dd29e2b9c7dfca847adc8d6
xbigcat <big.cat@qq.com>
2019-01-24 15:32:35 +0800

    perf[utils]: refine parseTime function (#1546)
    * 优化 parseTime
    
    修复传入的时间戳是字符串类型，不能转换时间的问题
    例：parseTime("1548221490638")
    
    * Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -11,7 +11,12 @@ export function parseTime(time, cFormat) {
   if (typeof time === 'object') {
     date = time
   } else {
-    if (('' + time).length === 10) time = parseInt(time) * 1000
+    if ((typeof time === 'string') && (/^[0-9]+$/.test(time))) {
+      time = parseInt(time)
+    }
+    if ((typeof time === 'number') && (time.toString().length === 10)) {
+      time = time * 1000
+    }
     date = new Date(time)
   }
   const formatObj = {
