ce7cb2f17facfb27f5ebd81591defce1beda0459
马云海 mayunhai () <mayunhai@tuniu.com>
2019-02-18 13:06:39 +0800

    用Object.assign()复制数组对象

--- src/utils/index.js
+++ src/utils/index.js
@@ -276,14 +276,7 @@ export function deepClone(source) {
   if (!source && typeof source !== 'object') {
     throw new Error('error arguments', 'shallowClone')
   }
-  const targetObj = source.constructor === Array ? [] : {}
-  Object.keys(source).forEach(keys => {
-    if (source[keys] && typeof source[keys] === 'object') {
-      targetObj[keys] = deepClone(source[keys])
-    } else {
-      targetObj[keys] = source[keys]
-    }
-  })
+  const targetObj = source.constructor === Array ? Object.assign([], source) : Object.assign({}, source)
   return targetObj
 }
 
