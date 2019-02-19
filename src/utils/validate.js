163a41fe8fba28ffe53e9938453ae2d0fa31eb2e
Pan <panfree23@gmail.com>
2019-02-19 10:07:18 +0800

    perf[utils]: optimize code

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -7,6 +7,10 @@ export function isvalidUsername(str) {
   return valid_map.indexOf(str.trim()) >= 0
 }
 
+export function isExternal(path) {
+  return /^(https?:|mailto:|tel:)/.test(path)
+}
+
 /* 合法uri*/
 export function validateURL(textval) {
   const urlregex = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/
--- src/views/layout/components/Sidebar/Link.vue
+++ src/views/layout/components/Sidebar/Link.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import { isExternal } from '@/utils'
+import { isExternal } from '@/utils/validate'
 
 export default {
   props: {
