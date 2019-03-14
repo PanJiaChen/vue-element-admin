87272fd62d7a1e46e121b4df92b32561c7a706f9
Yunfei <687418+OtaconBYF@users.noreply.github.com>
2019-03-14 16:12:47 +0800

    fix[utils]: fixed param2Obj not decoding plus sign (#1712)

--- src/utils/index.js
+++ src/utils/index.js
@@ -140,7 +140,8 @@ export function param2Obj(url) {
       decodeURIComponent(search)
         .replace(/"/g, '\\"')
         .replace(/&/g, '","')
-        .replace(/=/g, '":"') +
+        .replace(/=/g, '":"')
+        .replace(/\+/g, ' ') +
       '"}'
   )
 }
