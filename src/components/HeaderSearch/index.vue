966530722051cc4cfbfd5da18a8e89b55ca852d9
花裤衩 <panfree23@gmail.com>
2019-03-19 10:18:04 +0800

    fix[HeaderSearch]: fixed bug in vue2.6+ (#1733)

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="{'show':show}" class="header-search">
-    <svg-icon class-name="search-icon" icon-class="search" @click="click" />
+    <svg-icon class-name="search-icon" icon-class="search" @click.stop="click" />
     <el-select
       ref="headerSearchSelect"
       v-model="search"
