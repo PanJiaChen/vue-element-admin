8c091ce980cd6968e8f09d23d27aa3306f80cc56
Pan <panfree23@gmail.com>
2019-03-22 14:55:02 +0800

    init

---
+++ plop-templates/component/index.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  div
+  {{banana}}
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+})
+</script>
+<style lang="scss" scoped>
+
+</style>
