960b562e84360e692b0df6501b1bf390b552ed7c
Pan <panfree23@gmail.com>
2019-03-25 14:12:23 +0800

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -103,13 +103,13 @@ module.exports = {
       .when(process.env.NODE_ENV !== 'development',
         config => {
           config
-          // .plugin('ScriptExtHtmlWebpackPlugin')
-          // .after('html')
-          // .use('script-ext-html-webpack-plugin', [{
-          //   // `runtime` must same as runtimeChunk name. default is `runtime`
-          //   inline: /runtime\..*\.js$/
-          // }])
-          // .end()
+            .plugin('ScriptExtHtmlWebpackPlugin')
+            .after('html')
+            .use('script-ext-html-webpack-plugin', [{
+            // `runtime` must same as runtimeChunk name. default is `runtime`
+              inline: /runtime\..*\.js$/
+            }])
+            .end()
           config
             .optimization.splitChunks({
               chunks: 'all',
