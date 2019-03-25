326e89dda30f14486677baeae4ecb6cb5ed0dd28
Pan <panfree23@gmail.com>
2019-03-25 15:10:03 +0800

    refine

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="sidebar-logo-container">
+  <div class="sidebar-logo-container" :class="{'collapse':collapse}">
     <transition name="sidebarLogoFade">
       <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
-        <img :src="logo" class="sidebar-logo">
+        <img v-if="logo" :src="logo" class="sidebar-logo">
+        <h1 v-else class="sidebar-title">{{ title }} </h1>
       </router-link>
       <router-link v-else key="expand" class="sidebar-logo-link" to="/">
-        <img :src="logo" class="sidebar-logo">
+        <img v-if="logo" :src="logo" class="sidebar-logo">
         <h1 class="sidebar-title">{{ title }} </h1>
       </router-link>
     </transition>
@@ -57,17 +58,24 @@ export default {
       width: 32px;
       height: 32px;
       vertical-align: middle;
+      margin-right: 12px;
     }
 
     & .sidebar-title {
       display: inline-block;
-      margin: 0 0 0 12px;
+      margin: 0;
       color: #fff;
       font-weight: 600;
       line-height: 50px;
       font-size: 14px;
       font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
       vertical-align: middle;
+    }
+  }
+
+  &.collapse {
+    .sidebar-logo {
+      margin-right: 0px;
     }
   }
 }
