59508ba56f1b00d7d2e2a1d700f2860c1dc06c67
sun ao <sunao_0626@hotmail.com>
2019-03-26 20:24:19 +0800

    Update index.js
    确保不会responseType丢失

--- src/mock/index.js
+++ src/mock/index.js
@@ -11,6 +11,8 @@ Mock.XHR.prototype.proxy_send = Mock.XHR.prototype.send
 Mock.XHR.prototype.send = function() {
   if (this.custom.xhr) {
     this.custom.xhr.withCredentials = this.withCredentials || false
+    // 确保 responseType 不会丢失
+    this.custom.xhr['responseType'] = this['responseType']
   }
   this.proxy_send(...arguments)
 }
