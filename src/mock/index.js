4f7a1f12b21acfd054037b5a012fde3f307902c8
Pan <panfree23@gmail.com>
2019-03-27 13:30:23 +0800

    tweak

--- src/mock/index.js
+++ src/mock/index.js
@@ -11,8 +11,7 @@ Mock.XHR.prototype.proxy_send = Mock.XHR.prototype.send
 Mock.XHR.prototype.send = function() {
   if (this.custom.xhr) {
     this.custom.xhr.withCredentials = this.withCredentials || false
-    // 确保 responseType 不会丢失
-    this.custom.xhr['responseType'] = this['responseType']
+    this.custom.xhr.responseType = this.responseType
   }
   this.proxy_send(...arguments)
 }
