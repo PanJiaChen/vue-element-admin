0d40222b6420a4b4177067345b50ed8140982061
花裤衩 <panfree23@gmail.com>
2019-04-04 16:21:45 +0800

    fix[Drag]: fixed drag bug in firefox (#1841)

--- src/components/Kanban/index.vue
+++ src/components/Kanban/index.vue
@@ -7,6 +7,7 @@
       :list="list"
       v-bind="$attrs"
       class="board-column-content"
+      :set-data="setData"
     >
       <div v-for="element in list" :key="element.id" class="board-item">
         {{ element.name }} {{ element.id }}
@@ -38,6 +39,13 @@ export default {
       default() {
         return []
       }
+    }
+  },
+  methods: {
+    setData(dataTransfer) {
+      // to avoid Firefox bug
+      // Detail see : https://github.com/RubaXa/Sortable/issues/1012
+      dataTransfer.setData('Text', '')
     }
   }
 }
