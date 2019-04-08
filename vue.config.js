342b7b428a7e87951a03228bc2ee750cbb551eb9
Pan <panfree23@gmail.com>
2019-04-08 22:05:10 +0800

    fix[Chore]: add @vue/cli-plugin-eslint

--- vue.config.js
+++ vue.config.js
@@ -21,7 +21,7 @@ module.exports = {
   publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
-  lintOnSave: process.env.NODE_ENV === 'development' ? 'error' : false,
+  lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
     port: port,
