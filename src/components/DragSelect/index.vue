d0651ad2f23da312f0ec59011812966db668b6ce
Tuan Duong <bacduong@gmail.com>
2019-04-11 11:41:36 +0900

    perf: add jsdoc for utils and format some code (#1883)

--- src/components/DragSelect/index.vue
+++ src/components/DragSelect/index.vue
@@ -49,13 +49,13 @@ export default {
 </script>
 
 <style scoped>
-.drag-select >>> .sortable-ghost{
+.drag-select >>> .sortable-ghost {
   opacity: .8;
   color: #fff!important;
   background: #42b983!important;
 }
 
-.drag-select >>> .el-tag{
+.drag-select >>> .el-tag {
   cursor: pointer;
 }
 </style>
