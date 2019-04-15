f36b190e898eebc94c838197caa45895ebd54fb3
Pan <panfree23@gmail.com>
2019-04-15 11:48:29 +0800

    refine

--- src/permission.js
+++ src/permission.js
@@ -13,9 +13,10 @@ const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
 router.beforeEach(async(to, from, next) => {
   // start progress bar
   NProgress.start()
-  if (to.meta.title) {
-    document.title = getPageTitle(to.meta.title)
-  }
+
+  // set page title
+  document.title = getPageTitle(to.meta.title)
+
   // determine whether the user has logged in
   const hasToken = getToken()
 
