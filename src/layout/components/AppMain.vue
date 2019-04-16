061ab5ee6deea1395b91448575c48938422d364b
Pan <panfree23@gmail.com>
2019-04-16 16:19:04 +0800

    refine

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -25,7 +25,7 @@ export default {
 <style lang="scss" scoped>
 .app-main {
   /* 50= navbar  50  */
-  min-height: calc(100vh - 50px);
+  min-height: calc(100vh - 50px - 43px);
   width: 100%;
   position: relative;
   overflow: hidden;
@@ -38,7 +38,7 @@ export default {
 .hasTagsView {
   .app-main {
     /* 84 = navbar + tags-view = 50 + 34 */
-    min-height: calc(100vh - 84px);
+    min-height: calc(100vh - 84px - 43px);
   }
 
   .fixed-header+.app-main {
