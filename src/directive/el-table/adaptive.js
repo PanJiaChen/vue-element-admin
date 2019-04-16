e7275216315cc723526b7e4b4952f297decb7b4b
Pan <panfree23@gmail.com>
2019-04-16 14:54:09 +0800

    tweak

--- src/directive/el-table/adaptive.js
+++ src/directive/el-table/adaptive.js
@@ -1,11 +1,10 @@
-
 import { addResizeListener, removeResizeListener } from 'element-ui/src/utils/resize-event'
 
 /**
  * How to use
  * <el-table height="100px" v-el-height-adaptive-table="{bottomOffset: 30}">...</el-table>
  * el-table height is must be set
- *  bottomOffset: 30(default)   // The height of the table from the bottom of the page.
+ * bottomOffset: 30(default)   // The height of the table from the bottom of the page.
  */
 
 const doResize = (el, binding, vnode) => {
