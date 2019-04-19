909101729e216c86521a5f8cd3651b1174f16f5d
Pan <panfree23@gmail.com>
2019-04-19 18:14:32 +0800

    update

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import SvgIcon from '@/components/SvgIcon'// svg组件
+import SvgIcon from '@/components/SvgIcon'// svg component
 
 // register globally
 Vue.component('svg-icon', SvgIcon)
