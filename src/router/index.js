d4be15d9d9173667c3289c7d0bf991e5a8bc9cd8
Pan <panfree23@gmail.com>
2019-04-28 14:51:11 +0800

    perf: dashboard add cache

--- src/router/index.js
+++ src/router/index.js
@@ -79,7 +79,7 @@ export const constantRoutes = [
         path: 'dashboard',
         component: () => import('@/views/dashboard/index'),
         name: 'Dashboard',
-        meta: { title: 'dashboard', icon: 'dashboard', noCache: true, affix: true }
+        meta: { title: 'dashboard', icon: 'dashboard', affix: true }
       }
     ]
   },
