ad689e3f96e06820866fbe860c75d81643bf780f
Pan <panfree23@gmail.com>
2019-05-08 18:10:41 +0800

    perf[Style]: refine fixed-header style when open el-dialog

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -49,3 +49,11 @@ export default {
 }
 </style>
 
+<style lang="scss">
+// fix css style bug in open el-dialog
+.el-popup-parent--hidden {
+  .fixed-header {
+    padding-right: 15px;
+  }
+}
+</style>
