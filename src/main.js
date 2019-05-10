4ef0782189004e98cf427fd5eafc908d0a14a7d6
Pan <panfree23@gmail.com>
2019-05-10 14:47:57 +0800

    chore: use mockjs in production environment

--- src/main.js
+++ src/main.js
@@ -21,12 +21,16 @@ import * as filters from './filters' // global filters
 
 /**
  * If you don't want to use mock-server
- * you want to use mockjs for request interception
- * you can execute:
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
  *
- * import { mockXHR } from '../mock'
- * mockXHR()
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online! ! !
  */
+import { mockXHR } from '../mock'
+if (process.env.NODE_ENV === 'production') {
+  mockXHR()
+}
 
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium' // set element-ui default size
