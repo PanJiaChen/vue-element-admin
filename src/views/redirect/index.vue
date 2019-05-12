27df99791582b8a7a8cf1d35ec8a40ec2406ca33
花裤衩 <panfree23@gmail.com>
2019-05-12 14:01:04 +0800

    fix[redirect]: fix redirect bug in vue-devtools (#2066)

--- src/views/redirect/index.vue
+++ src/views/redirect/index.vue
@@ -4,6 +4,7 @@ export default {
     const { params, query } = this.$route
     const { path } = params
     this.$router.replace({ path: '/' + path, query })
+    return
   },
   render: function(h) {
     return h() // avoid warning message
