58bb0922b6def631f43cb29562e99d04514b463c
花裤衩 <panfree23@gmail.com>
2019-05-20 11:09:35 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -11,8 +11,7 @@ const modulesFiles = require.context('./modules', true, /\.js$/)
 // it will auto require all vuex module from modules file
 const modules = modulesFiles.keys().reduce((modules, modulePath) => {
   // set './app.js' => 'app'
-  var moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
-  moduleName = moduleName.substring(moduleName.indexOf('/') + 1)
+  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
   const value = modulesFiles(modulePath)
   modules[moduleName] = value.default
   return modules
