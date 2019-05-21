fd7160644a9c1a239fab7088819abf7fb06a9033
Pan <panfree23@gmail.com>
2019-05-21 10:23:19 +0800

    refine

--- src/components/Tinymce/index.vue
+++ src/components/Tinymce/index.vue
@@ -17,7 +17,6 @@ import plugins from './plugins'
 import toolbar from './toolbar'
 import load from './dynamicLoadScript'
 
-// inject tinymce into index.html
 // why use this cdn, detail see https://github.com/PanJiaChen/tinymce-all-in-one
 const tinymceCDN = 'https://cdn.jsdelivr.net/npm/tinymce-all-in-one@4.9.3/tinymce.min.js'
 
@@ -102,6 +101,7 @@ export default {
   },
   methods: {
     init() {
+      // dynamic load tinymce from cdn
       load(tinymceCDN, (err) => {
         if (err) {
           this.$message.error(err.message)
