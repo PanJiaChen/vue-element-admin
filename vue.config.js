6a322c9defbcbf2adffacc7141022baf4be10e33
花裤衩 <panfree23@gmail.com>
2019-05-26 17:12:36 +0800

    chore: set localhost => 127.0.0.1 (#2142)

--- vue.config.js
+++ vue.config.js
@@ -37,7 +37,7 @@ module.exports = {
       // change xxx-api/login => mock/login
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:${port}/mock`,
+        target: `http://127.0.0.1:${port}/mock`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
