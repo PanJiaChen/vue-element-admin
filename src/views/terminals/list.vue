48c2c14e94e4bbb2dd95d30e619ed9a0262068ef
Chad Derya <scd@onlinefuels.com>
2019-09-12 11:34:52 +0100

    hide full name of a terminal from DE

--- src/views/terminals/list.vue
+++ src/views/terminals/list.vue
@@ -14,7 +14,7 @@
           <span>{{ scope.row.name }}</span>
         </template>
       </el-table-column>
-      <el-table-column align="center" label="FullName" width="200">
+      <el-table-column v-if="$store.state.settings.platform === 'OLFDE'" align="center" label="FullName" width="200">
         <template slot-scope="scope">
           <span>{{ scope.row.fullName }}</span>
         </template>
