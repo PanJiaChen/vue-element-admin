567aa052e1f6af4916517b052e396c88653c514c
花裤衩 <panfree23@gmail.com>
2019-10-08 20:33:44 +0800

    fix[logout]: empty tagsview when logout

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -73,13 +73,18 @@ const actions = {
   },
 
   // user logout
-  logout({ commit, state }) {
+  logout({ commit, state, dispatch }) {
     return new Promise((resolve, reject) => {
       logout(state.token).then(() => {
         commit('SET_TOKEN', '')
         commit('SET_ROLES', [])
         removeToken()
         resetRouter()
+
+        // reset visited views and cached views
+        // to fixed https://github.com/PanJiaChen/vue-element-admin/issues/2485
+        dispatch('tagsView/delAllViews', null, { root: true })
+
         resolve()
       }).catch(error => {
         reject(error)
