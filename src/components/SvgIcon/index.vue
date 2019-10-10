ce667850cf80094e40e8ebdbfaa20e1f5b7f1c0c
MaYuanhai <414199639@qq.com>
2019-10-10 11:35:48 +0800

    perf[SvgIcon]: change xlink:href to href(#2645)
    https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/xlink:href

--- src/components/SvgIcon/index.vue
+++ src/components/SvgIcon/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="isExternal" :style="styleExternalIcon" class="svg-external-icon svg-icon" v-on="$listeners" />
   <svg v-else :class="svgClass" aria-hidden="true" v-on="$listeners">
-    <use :xlink:href="iconName" />
+    <use :href="iconName" />
   </svg>
 </template>
 
