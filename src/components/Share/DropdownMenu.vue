c4d17bec4d83e2217a433749fc0114fc978b09c0
Phạm Ngọc Hòa <thaycacac@gmail.com>
2019-11-05 10:19:26 +0700

    format: dropdown menu

--- src/components/Share/DropdownMenu.vue
+++ src/components/Share/DropdownMenu.vue
@@ -90,7 +90,7 @@ $t: .1s;
     .share-dropdown-menu-item {
       @for $i from 1 through $n {
         &:nth-of-type(#{$i}) {
-         transition-delay: ($n - $i)*$t;
+          transition-delay: ($n - $i)*$t;
           transform: translate3d(0, ($i - 1)*60px, 0);
         }
       }
