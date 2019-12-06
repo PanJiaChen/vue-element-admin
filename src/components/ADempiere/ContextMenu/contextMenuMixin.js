7372a21308d1661ea9f3c259b0b0427994112a86
Leonel Matos <matosleonel0@gmail.com>
2019-12-06 00:33:02 -0400

    add validation for private access records (#195)

--- src/components/ADempiere/ContextMenu/contextMenuMixin.js
+++ src/components/ADempiere/ContextMenu/contextMenuMixin.js
@@ -275,15 +275,17 @@ export const contextMixin = {
     },
     generateContextMenu() {
       this.metadataMenu = this.getterContextMenu
-      this.$store.dispatch('getPrivateAccessFromServer', {
-        tableName: this.$route.params.tableName,
-        recordId: this.$route.params.recordId
-      })
-        .then(response => {
-          this.$nextTick(() => {
-            this.validatePrivateAccess(response)
-          })
+      if (this.panelType === 'window') {
+        this.$store.dispatch('getPrivateAccessFromServer', {
+          tableName: this.$route.params.tableName,
+          recordId: this.$route.params.recordId
         })
+          .then(response => {
+            this.$nextTick(() => {
+              this.validatePrivateAccess(response)
+            })
+          })
+      }
       this.actions = this.metadataMenu.actions
 
       if (this.actions && this.actions.length) {
