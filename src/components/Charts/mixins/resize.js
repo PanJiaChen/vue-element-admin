e138bbe9c14a87035a647de7fec6055bf36e5354
花裤衩 <panfree23@gmail.com>
2020-01-08 17:59:42 +0800

    fix[Charts]: fix charts resize in keep-alive bug

--- src/components/Charts/mixins/resize.js
+++ src/components/Charts/mixins/resize.js
@@ -7,20 +7,17 @@ export default {
     }
   },
   mounted() {
-    this.__resizeHandler = debounce(() => {
-      if (this.chart) {
-        this.chart.resize()
-      }
-    }, 100)
-    window.addEventListener('resize', this.__resizeHandler)
-
-    this.$_sidebarElm = document.getElementsByClassName('sidebar-container')[0]
-    this.$_sidebarElm && this.$_sidebarElm.addEventListener('transitionend', this.$_sidebarResizeHandler)
+    this.initListener()
+  },
+  activated() {
+    this.resize()
+    this.initListener()
   },
   beforeDestroy() {
-    window.removeEventListener('resize', this.__resizeHandler)
-
-    this.$_sidebarElm && this.$_sidebarElm.removeEventListener('transitionend', this.$_sidebarResizeHandler)
+    this.destroyListener()
+  },
+  deactivated() {
+    this.destroyListener()
   },
   methods: {
     // use $_ for mixins properties
@@ -28,6 +25,25 @@ export default {
     $_sidebarResizeHandler(e) {
       if (e.propertyName === 'width') {
         this.__resizeHandler()
+      }
+    },
+    initListener() {
+      this.__resizeHandler = debounce(() => {
+        this.resize()
+      }, 100)
+      window.addEventListener('resize', this.__resizeHandler)
+
+      this.$_sidebarElm = document.getElementsByClassName('sidebar-container')[0]
+      this.$_sidebarElm && this.$_sidebarElm.addEventListener('transitionend', this.$_sidebarResizeHandler)
+    },
+    destroyListener() {
+      window.removeEventListener('resize', this.__resizeHandler)
+
+      this.$_sidebarElm && this.$_sidebarElm.removeEventListener('transitionend', this.$_sidebarResizeHandler)
+    },
+    resize() {
+      if (this.chart) {
+        this.chart.resize()
       }
     }
   }
