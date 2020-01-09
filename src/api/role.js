025d5e46a2e8bdafeda46da20a5b756b6163860e
花裤衩 <panfree23@gmail.com>
2020-01-09 17:21:36 +0800

    fix: fixed mock server (#2929)
    * init
    
    * refine
    
    * update

--- src/api/role.js
+++ src/api/role.js
@@ -2,21 +2,21 @@ import request from '@/utils/request'
 
 export function getRoutes() {
   return request({
-    url: '/routes',
+    url: '/vue-element-admin/routes',
     method: 'get'
   })
 }
 
 export function getRoles() {
   return request({
-    url: '/roles',
+    url: '/vue-element-admin/roles',
     method: 'get'
   })
 }
 
 export function addRole(data) {
   return request({
-    url: '/role',
+    url: '/vue-element-admin/role',
     method: 'post',
     data
   })
@@ -24,7 +24,7 @@ export function addRole(data) {
 
 export function updateRole(id, data) {
   return request({
-    url: `/role/${id}`,
+    url: `/vue-element-admin/role/${id}`,
     method: 'put',
     data
   })
@@ -32,7 +32,7 @@ export function updateRole(id, data) {
 
 export function deleteRole(id) {
   return request({
-    url: `/role/${id}`,
+    url: `/vue-element-admin/role/${id}`,
     method: 'delete'
   })
 }
