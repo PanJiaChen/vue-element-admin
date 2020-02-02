4a2401b5741c2ea3efd8ee44fca9b14946da86bf
RoBlues <robluesmail@gmail.com>
2020-02-02 19:39:41 +0800

    perf[Mock]: set responseFake to mock-server.js(#2966)
    * modify 把mock/index.js中与mock-server相关的代码转移到mock-server.js中去，实现两则功能分割

--- mock/index.js
+++ mock/index.js
@@ -54,18 +54,4 @@ export function mockXHR() {
   }
 }
 
-// for mock server
-const responseFake = (url, type, respond) => {
-  return {
-    url: new RegExp(`${process.env.VUE_APP_BASE_API}${url}`),
-    type: type || 'get',
-    response(req, res) {
-      console.log('request invoke:' + req.path)
-      res.json(Mock.mock(respond instanceof Function ? respond(req, res) : respond))
-    }
-  }
-}
-
-export default mocks.map(route => {
-  return responseFake(route.url, route.type, route.response)
-})
+export default mocks
