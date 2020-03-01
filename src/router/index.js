a4d771f5f5adccfeaf577aae7213cdc64af69d65
feaswcy <708853816@qq.com>
2020-03-01 22:23:57 +0800

    fix: use router.matched
    according to vue-router's document, router.matcher should be router.matched

--- src/router/index.js
+++ src/router/index.js
@@ -398,7 +398,7 @@ const router = createRouter()
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
   const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
+  router.matched = newRouter.matched // reset router
 }
 
 export default router
