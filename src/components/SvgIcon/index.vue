b54ced1ff529198be4fa6302724212f4386551cf
花裤衩 <panfree23@gmail.com>
2020-03-28 11:12:39 +0800

    fix: svg support old broswer

--- src/components/SvgIcon/index.vue
+++ src/components/SvgIcon/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="isExternal" :style="styleExternalIcon" class="svg-external-icon svg-icon" v-on="$listeners" />
   <svg v-else :class="svgClass" aria-hidden="true" v-on="$listeners">
-    <use :href="iconName" />
+    <use :xlink:href="iconName" />
   </svg>
 </template>
 
