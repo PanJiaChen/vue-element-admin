14e655ffe45092f6693c08939e5529f50b19d370
MaYuanhai <414199639@qq.com>
2020-04-10 11:32:05 +0800

    Update param2Obj.spec.js

--- tests/unit/utils/param2Obj.spec.js
+++ tests/unit/utils/param2Obj.spec.js
@@ -2,7 +2,7 @@ import { param2Obj } from '@/utils/index.js'
 describe('Utils:param2Obj', () => {
   const url = 'https://github.com/PanJiaChen/vue-element-admin?name=bill&age=29&sex=1&field=dGVzdA=='
 
-  it('param2Obj', () => {
+  it('param2Obj test', () => {
     expect(param2Obj(url)).toEqual({
       name: 'bill',
       age: '29',
