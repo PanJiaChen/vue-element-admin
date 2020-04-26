4d9854cd6a45a3765678f8eac566f26fdd7db365
aisen60 <caixusheng60@gmail.com>
2020-04-26 23:56:12 +0800

    try to keep these URLs intact

--- src/components/Tinymce/index.vue
+++ src/components/Tinymce/index.vue
@@ -147,7 +147,11 @@ export default {
           editor.on('FullscreenStateChanged', (e) => {
             _this.fullscreen = e.state
           })
-        }
+        },
+        //it will try to keep these URLs intact
+        //https://www.tiny.cloud/docs-3x/reference/configuration/Configuration3x@convert_urls/
+        //https://stackoverflow.com/questions/5196205/disable-tinymce-absolute-to-relative-url-conversions
+        convert_urls: false
         // 整合七牛上传
         // images_dataimg_filter(img) {
         //   setTimeout(() => {
