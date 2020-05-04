aeb33b1ef9e9a29a17d4f5158fd0816c192832b2
花裤衩 <panfree23@gmail.com>
2020-05-04 21:49:53 +0800

    perf: refine name && tips

--- src/main.js
+++ src/main.js
@@ -4,10 +4,9 @@ import Cookies from 'js-cookie'
 
 import 'normalize.css/normalize.css' // a modern alternative to CSS resets
 
-import elementLocale from 'element-ui/lib/locale/lang/en'
-
 import Element from 'element-ui'
 import './styles/element-variables.scss'
+import enLang from 'element-ui/lib/locale/lang/en'// 如果使用中文语言包请默认支持，无需额外引入，请删除该依赖
 
 import '@/styles/index.scss' // global css
 
@@ -36,7 +35,7 @@ if (process.env.NODE_ENV === 'production') {
 
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium', // set element-ui default size
-  locale: elementLocale
+  locale: enLang // 如果使用中文，无需设置，请删除
 })
 
 // register global utility filters
