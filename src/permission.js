e692876c289a227b92476b13b355987a18b59422
花裤衩 <panfree23@gmail.com>
2020-05-05 11:06:51 +0800

    docs: add tips

--- src/permission.js
+++ src/permission.js
@@ -24,7 +24,7 @@ router.beforeEach(async(to, from, next) => {
     if (to.path === '/login') {
       // if is logged in, redirect to the home page
       next({ path: '/' })
-      NProgress.done()
+      NProgress.done() // hack: https://github.com/PanJiaChen/vue-element-admin/pull/2939
     } else {
       // determine whether the user has obtained his permission roles through getInfo
       const hasRoles = store.getters.roles && store.getters.roles.length > 0
