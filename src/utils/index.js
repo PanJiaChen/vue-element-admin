5890499077589db1c04c7a72c4dacaa97a58f87c
cooper <1322849632@qq.com>
2020-05-05 13:21:51 +0800

    fix[parseTime]: fixed when pass null (#3038)

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,7 +9,7 @@
  * @returns {string | null}
  */
 export function parseTime(time, cFormat) {
-  if (arguments.length === 0) {
+  if (arguments.length === 0 || !time) {
     return null
   }
   const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
