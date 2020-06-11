e9419b6277d876d77c83d44bcec0114b8013fbb3
花裤衩 <panfree23@gmail.com>
2020-06-11 20:51:10 +0800

    perf: lint code

--- src/utils/index.js
+++ src/utils/index.js
@@ -162,21 +162,21 @@ export function param(json) {
  * @returns {Object}
  */
 export function param2Obj(url) {
-    const search = decodeURIComponent(url.split('?')[1]).replace(/\+/g, ' ')
-    if (!search) {
-        return {}
+  const search = decodeURIComponent(url.split('?')[1]).replace(/\+/g, ' ')
+  if (!search) {
+    return {}
+  }
+  const obj = {}
+  const searchArr = search.split('&')
+  searchArr.forEach(v => {
+    const index = v.indexOf('=')
+    if (index !== -1) {
+      const name = v.substring(0, index)
+      const val = v.substring(index + 1, v.length)
+      obj[name] = val
     }
-    const obj = {}
-    const searchArr = search.split('&')
-    searchArr.forEach(v => {
-        const index = v.indexOf('=')
-        if (index !== -1) {
-            const name = v.substring(0, index)
-            const val = v.substring(index + 1, v.length)
-            obj[name] = val
-        }
-    })
-    return obj
+  })
+  return obj
 }
 
 /**
