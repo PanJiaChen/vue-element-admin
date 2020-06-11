4e7665c07226cf3bc3278a9af1bf60fe6782cb7d
花裤衩 <panfree23@gmail.com>
2020-06-11 21:02:18 +0800

    fix[v-permission]: support dynamic set roles (#3251)

--- src/directive/permission/permission.js
+++ src/directive/permission/permission.js
@@ -1,11 +1,11 @@
 import store from '@/store'
 
-export default {
-  inserted(el, binding, vnode) {
-    const { value } = binding
-    const roles = store.getters && store.getters.roles
+function checkPermission(el, binding) {
+  const { value } = binding
+  const roles = store.getters && store.getters.roles
 
-    if (value && value instanceof Array && value.length > 0) {
+  if (value && value instanceof Array) {
+    if (value.length > 0) {
       const permissionRoles = value
 
       const hasPermission = roles.some(role => {
@@ -15,8 +15,17 @@ export default {
       if (!hasPermission) {
         el.parentNode && el.parentNode.removeChild(el)
       }
-    } else {
-      throw new Error(`need roles! Like v-permission="['admin','editor']"`)
     }
+  } else {
+    throw new Error(`need roles! Like v-permission="['admin','editor']"`)
+  }
+}
+
+export default {
+  inserted(el, binding) {
+    checkPermission(el, binding)
+  },
+  update(el, binding) {
+    checkPermission(el, binding)
   }
 }
