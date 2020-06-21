81a14524d1abb0bf6480301c0dc24afd1e39cad2
Dihak <dihak@digitalkode.com>
2020-06-21 20:25:55 +0700

    bump: update to vue-cli@4 (#3028)

--- src/views/excel/merge-header.vue
+++ src/views/excel/merge-header.vue
@@ -71,21 +71,21 @@ export default {
     },
     handleDownload() {
       this.downloadLoading = true
-        import('@/vendor/Export2Excel').then(excel => {
-          const multiHeader = [['Id', 'Main Information', '', '', 'Date']]
-          const header = ['', 'Title', 'Author', 'Readings', '']
-          const filterVal = ['id', 'title', 'author', 'pageviews', 'display_time']
-          const list = this.list
-          const data = this.formatJson(filterVal, list)
-          const merges = ['A1:A2', 'B1:D1', 'E1:E2']
-          excel.export_json_to_excel({
-            multiHeader,
-            header,
-            merges,
-            data
-          })
-          this.downloadLoading = false
+      import('@/vendor/Export2Excel').then(excel => {
+        const multiHeader = [['Id', 'Main Information', '', '', 'Date']]
+        const header = ['', 'Title', 'Author', 'Readings', '']
+        const filterVal = ['id', 'title', 'author', 'pageviews', 'display_time']
+        const list = this.list
+        const data = this.formatJson(filterVal, list)
+        const merges = ['A1:A2', 'B1:D1', 'E1:E2']
+        excel.export_json_to_excel({
+          multiHeader,
+          header,
+          merges,
+          data
         })
+        this.downloadLoading = false
+      })
     },
     formatJson(filterVal, jsonData) {
       return jsonData.map(v => filterVal.map(j => {
