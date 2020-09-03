3db7d3f910cb5bd0958534c53f006b85b99acd99
Edwin Betancourt <edwinbetanc0urt@hotmail.com>
2020-09-03 14:31:14 -0400

    feat: Update and replace runjs with taskfile.

--- build/index.js
+++ build/index.js
@@ -1,15 +1,12 @@
-const { run } = require('runjs')
+const { sh } = require('tasksfile')
 const chalk = require('chalk')
 const config = require('../vue.config.js')
 const rawArgv = process.argv.slice(2)
 const args = rawArgv.join(' ')
 
+sh(`vue-cli-service build ${args}`)
+
 if (process.env.npm_config_preview || rawArgv.includes('--preview')) {
-  const report = rawArgv.includes('--report')
-
-  run(`vue-cli-service build ${args}`)
-
-  const port = 9526
   const publicPath = config.publicPath
 
   var connect = require('connect')
@@ -23,13 +20,14 @@ if (process.env.npm_config_preview || rawArgv.includes('--preview')) {
     })
   )
 
+  const port = 9526
   app.listen(port, function () {
     console.log(chalk.green(`> Preview at  http://localhost:${port}${publicPath}`))
-    if (report) {
+
+    const isWithReport = rawArgv.includes('--report')
+    if (isWithReport) {
       console.log(chalk.green(`> Report at  http://localhost:${port}${publicPath}report.html`))
     }
 
   })
-} else {
-  run(`vue-cli-service build ${args}`)
 }
