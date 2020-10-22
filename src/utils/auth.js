f6bd99d718fc3c641f1f613ffaf7b4bfda016e1e
Anas El Alami <anaselalamikh@gmail.com>
2020-10-22 17:45:19 +0200

    Adding { SameSite: 'Lax' } to Cookies.set attributes

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -7,7 +7,7 @@ export function getToken() {
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return Cookies.set(TokenKey, token, { SameSite: 'Lax' })
 }
 
 export function removeToken() {
