078b9256c7a76479ab4209eb77b90f73436d1486
Edwin Betancourt <EdwinBetanc0urt@hotmail.com>
2020-10-30 12:24:12 -0400

    Update constants.js (#531)
    Change of the environment variables (`VUE_APP_API_REST_ADDRESS`) values.
    
    
    https://github.com/adempiere/adempiere-vue-docker/pull/4

--- src/api/ADempiere/constants.js
+++ src/api/ADempiere/constants.js
@@ -1,5 +1,5 @@
 
-const apiRestAddress = process.env.VUE_APP_API_REST_ADDRESS || '0.0.0.0'
+const apiRestAddress = process.env.VUE_APP_API_REST_ADDRESS || 'http://localhost'
 const apiRestPort = process.env.VUE_APP_API_REST_PORT || '8085'
 
-export const API_REST_ADDRESS = `http://${apiRestAddress}:${apiRestPort}/adempiere-api`
+export const API_REST_ADDRESS = `${apiRestAddress}:${apiRestPort}/adempiere-api`
