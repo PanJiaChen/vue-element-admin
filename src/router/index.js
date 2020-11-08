d43b55966ebabc395debd138337454cb19fd0924
D See Ker <dseeker@users.noreply.github.com>
2020-11-08 22:41:30 +0000

    #3289 add base path to match publicPath
    fixes breaking to 404 page in history mode when publicPath sub path is set

--- src/router/index.js
+++ src/router/index.js
@@ -389,6 +389,7 @@ export const asyncRoutes = [
 
 const createRouter = () => new Router({
   // mode: 'history', // require service support
+  // base: process.env.VUE_APP_SRC, // public
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
