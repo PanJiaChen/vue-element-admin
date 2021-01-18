3d21406015b325e31ed561059ac51352555081ac
zyxwvu Shi <i@shiyc.cn>
2021-01-18 06:18:31 +0000

    Replace CSS calc with box-sizing for better compatibility.
    Signed-off-by: zyxwvu Shi <i@shiyc.cn>

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -24,8 +24,8 @@ export default {
 
 <style lang="scss" scoped>
 .app-main {
-  /* 50= navbar  50  */
-  min-height: calc(100vh - 50px);
+  min-height: 100vh;
+  box-sizing: border-box;
   width: 100%;
   position: relative;
   overflow: hidden;
@@ -36,11 +36,6 @@ export default {
 }
 
 .hasTagsView {
-  .app-main {
-    /* 84 = navbar + tags-view = 50 + 34 */
-    min-height: calc(100vh - 84px);
-  }
-
   .fixed-header+.app-main {
     padding-top: 84px;
   }
