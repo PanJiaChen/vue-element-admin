951e1c56a472dea65d7f6b8a963a3c47fb77563b
995928524 <995928524@qq.com>
2021-02-07 17:51:26 +0800

    updata

--- plopfile.js
+++
@@ -1,9 +0,0 @@
-const viewGenerator = require('./plop-templates/view/prompt')
-const componentGenerator = require('./plop-templates/component/prompt')
-const storeGenerator = require('./plop-templates/store/prompt.js')
-
-module.exports = function(plop) {
-  plop.setGenerator('view', viewGenerator)
-  plop.setGenerator('component', componentGenerator)
-  plop.setGenerator('store', storeGenerator)
-}
