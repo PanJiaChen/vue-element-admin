906ff72abcf942d1b484ac84941e0fbc3a031429
Elsio Sanchez <45974454+elsiosanchez@users.noreply.github.com>
2021-05-31 18:47:28 -0400

    style of the product information table in mobile mode (#890)
    Co-authored-by: elsiosanchez <elsiossanches@gmail.com>

--- src/components/ADempiere/Form/ProductInfo/productList.vue
+++ src/components/ADempiere/Form/ProductInfo/productList.vue
@@ -18,6 +18,7 @@
 <template>
   <el-main
     v-shortkey="shortsKey"
+    style="height: 90vh;"
   >
     <el-form
       v-shortkey="shortsKey"
@@ -38,7 +39,7 @@
       :data="listWithPrice"
       border
       fit
-      :height="isMobile ? '300' : '550'"
+      class="table-product-info"
       highlight-current-row
       @row-click="findlistProductWithRow"
       @current-change="handleCurrentChange"
@@ -420,3 +421,10 @@ export default {
   }
 }
 </script>
+<style>
+.table-product-info {
+  max-height: 80%;
+  min-height: 80%;
+  overflow: auto
+}
+</style>
