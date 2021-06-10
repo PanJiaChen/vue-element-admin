686e44564aed2c02b57a6356cfb1095a2266b867
yeke1230 <yeke1230@gmail.com>
2021-06-10 09:45:55 +0800

    initial commit

--- src/views/error-log/index.vue
+++ src/views/error-log/index.vue
@@ -4,9 +4,9 @@
     <ErrorB />
     <h3>Please click the bug icon in the upper right corner</h3>
     <aside>
-      Now the management system are basically the form of the spa, it enhances the user experience, but it also increases the possibility of page problems, a small negligence may lead to the entire page deadlock. Fortunately Vue provides a way to catch handling exceptions, where you can handle errors or report exceptions.
+      asddasf Now the management system are basically the form of the spa, it enhances the user experience, but it also increases the possibility of page problems, a small negligence may lead to the entire page deadlock. Fortunately Vue provides a way to catch handling exceptions, where you can handle errors or report exceptions.
       <a target="_blank" class="link-type" href="https://panjiachen.github.io/vue-element-admin-site/guide/advanced/error.html">
-        Document introduction
+        Document introduction safsadfasd 
       </a>
     </aside>
     <a href="#">
