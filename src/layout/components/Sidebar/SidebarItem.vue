16db3d668307250a0fc5841d6cd4229bf812a53d
Elsio Sanchez <45974454+elsiosanchez@users.noreply.github.com>
2021-06-18 13:27:31 -0400

    Fix Sidebar (#923)
    Co-authored-by: elsiosanchez <elsiossanches@gmail.com>

--- src/layout/components/Sidebar/SidebarItem.vue
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -8,7 +8,13 @@
           :class="{'submenu-title-noDropdown':!isNest}"
           @click="openItemMenu"
         >
+          <el-tooltip v-if="!sidebar.opened && !isNest" effect="dark" :content="onlyOneChild.meta.title" placement="right">
+            <item
+              :icon="onlyOneChild.meta.icon || (item.meta && item.meta.icon)"
+            />
+          </el-tooltip>
           <item
+            v-else
             :icon="onlyOneChild.meta.icon || (item.meta && item.meta.icon)"
             :title="generateTitle(onlyOneChild.meta.title)"
           />
@@ -18,7 +24,10 @@
 
     <el-submenu v-else ref="subMenu" :index="resolvePath(item.path)" popper-append-to-body>
       <template slot="title">
-        <item v-if="item.meta" :icon="item.meta && item.meta.icon" :title="generateTitle(item.meta.title)" />
+        <item v-if="sidebar.opened" :icon="item.meta && item.meta.icon" :title="generateTitle(item.meta.title)" />
+        <el-tooltip v-else effect="dark" :content="item.meta.title" placement="top-start">
+          <item v-if="item.meta && !isNest" :icon="item.meta && item.meta.icon" />
+        </el-tooltip>
       </template>
       <sidebar-item
         v-for="child in item.children"
