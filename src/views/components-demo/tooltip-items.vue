98c4fcc776a7fb719f8e210507a45aa9ab28ee00
SCIPLUS\yyding <yahya_dyy@163.com>
2021-07-08 16:51:13 +0800

    备注： 1.新建tooltip-item组件,用于详情展示，内容部分超出...鼠标放上自动打开tooltip插件显示完成内容，无...鼠标放上无效果 2.添加演示新组件页面tooltip-items

---
+++ src/views/components-demo/tooltip-items.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="components-container">
+    <el-card class="box-card">
+      <el-row :gutter="10">
+        <el-col :span="6">
+          <tooltip-item label="申请人：" content="张三，李四，王二，张伟，大鹏，小雕" />
+        </el-col>
+        <el-col :span="6">
+          <tooltip-item label="发明人：" content="张三，李四，王二" />
+        </el-col>
+        <el-col :span="6">
+          <tooltip-item label="申请日期：" content="2020-07-08" />
+        </el-col>
+        <el-col :span="6">
+          <tooltip-item label="IPC分类号：" :label-width="92" content="DC12312312412312312412314124" />
+        </el-col>
+      </el-row>
+      <el-row :gutter="10">
+        <el-col :span="8">
+          <tooltip-item label="地址：" content="上海市徐汇区上海市徐汇区上海市徐汇区上海市徐汇区上海市徐汇区" />
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+<script>
+import TooltipItem from '@/components/TooltipItem'
+export default {
+  name: 'TooltipItems',
+  components: { TooltipItem }
+}
+</script>
+
+<style scoped />
