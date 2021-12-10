ff69e18d66fe3928cdbc716c41c1b509473588f7
baedyl <emmanuelabona@gmail.com>
2021-12-10 14:05:02 +0100

    adding dist subtree

---
+++ dist/static/js/chunk-2d230fe7.3bd3f709.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d230fe7"],{ef3c:function(e,r,n){"use strict";n.r(r);n("ac1f"),n("5319");var t,u,a={created:function(){var e=this.$route,r=e.params,n=e.query,t=r.path;this.$router.replace({path:"/"+t,query:n})},render:function(e){return e()}},c=a,o=n("2877"),p=Object(o["a"])(c,t,u,!1,null,null,null);r["default"]=p.exports}}]);
