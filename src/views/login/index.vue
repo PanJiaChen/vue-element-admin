46bc7878d3003bad2a8b50cc29efbf9808567a94
projoin <106225379+projoin@users.noreply.github.com>
2022-05-25 14:06:48 +0800

    rerwerw tewer

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,7 +9,7 @@
       label-position="left"
     >
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title"系统</h3>
       </div>
 
       <el-form-item prop="username">
