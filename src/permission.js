104aab9efab30540520a4ae8f0592e624cfb3541
hezhouhang <hezhouhang@haodf.com>
2022-06-21 15:44:30 +0800

    fix: query lost when redirect

--- src/permission.js
+++ src/permission.js
@@ -49,7 +49,7 @@ router.beforeEach(async(to, from, next) => {
           // remove token and go to login page to re-login
           await store.dispatch('user/resetToken')
           Message.error(error || 'Has Error')
-          next(`/login?redirect=${to.path}`)
+          next(`/login?redirect=${to.fullPath}`)
           NProgress.done()
         }
       }
@@ -62,7 +62,7 @@ router.beforeEach(async(to, from, next) => {
       next()
     } else {
       // other pages that do not have permission to access are redirected to the login page.
-      next(`/login?redirect=${to.path}`)
+      next(`/login?redirect=${to.fullPath}`)
       NProgress.done()
     }
   }
